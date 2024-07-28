4244be83ca40089c727d715aa2fe598b22aacf5e
xiaolinzi7118 <1106147743@qq.com>
2024-07-28 23:48:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import myButton from './src/components/myButton/index.vue'
+import 'vant/lib/index.css';
+export default myButton
---
+++ src/components/myButton/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Button v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </Button>
+</template>
+
+<script setup>
+import { Button } from 'vant';
+</script>
